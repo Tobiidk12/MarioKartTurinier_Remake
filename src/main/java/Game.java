90305930cb2f25e7e438119a5300407a90bd34b9
Tobi <puschtobi@gmail.com>
2025-04-24 20:06:59 +0200

    added stuff

---
+++ src/main/java/Game.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Game
+{
+public HashMap<Integer , Player> spielerListe = new HashMap<Integer , Player>();
+
+
+
+
+
+
+
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+
+
+    private String name;
+    private int points;
+    private int place;
+    private boolean active;
+
+
+
+}
